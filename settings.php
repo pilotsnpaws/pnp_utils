0e0ee45a45853f322ceb2edbc687af3149b39572
nekbet <nekbet@gmail.com>
2016-07-26 17:52:11 -0400

    settin's

--- settings.php
+++ settings.php
@@ -4,5 +4,5 @@ $environment = 'dev' ;
 $configPath  = 'ca_dev/local_config.php' ;
 $logPath	= '/dev/null' ; 
 // $sgApiKey = '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'; // production key
-
+ 
 ?>
