1eb75917f74184c339b38d5d88cf992240f262f8
nekbet <nekbet@gmail.com>
2016-07-26 17:54:56 -0400

    foo

--- settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$environment = 'dev' ;
-$configPath  = 'ca_dev/local_config.php' ;
-$logPath	= '/dev/null' ; 
-// $sgApiKey = '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'; // production key
- 
-?>
